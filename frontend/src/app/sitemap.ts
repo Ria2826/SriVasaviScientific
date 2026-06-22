7553612927c3c9604a7fcc2803209e9da4beadaf
Ria2826 <riakrishnakumar@gmail.com>
2026-06-22 21:52:02 +0530

    Added sitemap

---
+++ frontend/src/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://sri-vasavi-scientific.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sri-vasavi-scientific.vercel.app/products",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sri-vasavi-scientific.vercel.app/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://sri-vasavi-scientific.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
