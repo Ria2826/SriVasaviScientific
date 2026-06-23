e79893f2a88d00cc1b9d8df352098af4ce86ce8b
Ria2826 <riakrishnakumar@gmail.com>
2026-06-23 16:40:19 +0530

    Fix sitemap deployment

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -5,26 +5,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://sri-vasavi-scientific.vercel.app",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/products",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/about",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/contact",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
     },
   ];
 }
