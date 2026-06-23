344dc1255ccdbf47d7dff2e6e061080172bd5f87
Ria2826 <riakrishnakumar@gmail.com>
2026-06-23 11:49:27 +0530

    Fixed sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,20 +1,30 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://sri-vasavi-scientific.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/products",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/about",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: "https://sri-vasavi-scientific.vercel.app/contact",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
   ];
 }
